1a75a52cb58eef4562bd0c2c4ff54ad147dc680c
Alder-R <kdh012356@gmail.com>
2024-06-20 09:58:15 +0900

    수업 문제 풀이 작성

--- myProject/src/myFinalTest/Test1.java
+++ myProject/src/myFinalTest/Test1.java
@@ -1,11 +1,20 @@
-
 package myFinalTest;
 
-public class Test1 {
+class RecClass {
+	double area(int x) {		// 정사각형의 면적 구하기
+		return x = x*x;
+	}
+	double round(int y) {		// 정사각형의 둘레 구하기
+		return y = y*4;
+	}
+}
+
+public class Test1 extends RecClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		RecClass rc = new RecClass();
+		System.out.println("정사각형의 면적은: " + rc.area(5));
+		System.out.println("정사각형의 둘레는: " + rc.round(5));
 	}
 
 }
