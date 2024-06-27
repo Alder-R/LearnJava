ce7ad2591334c2b744b6c7d0a8ff7085a1d697e8
Alder-R <kdh012356@gmail.com>
2024-06-27 11:45:41 +0900

    Math 클래스 활용: 피타고라스 정리로 거리 구하기

---
+++ myProject_re/src/myTest1/Pythagoras.java
@@ -0,0 +1,11 @@
+package myTest1;
+
+public class Pythagoras {
+
+	public static void main(String[] args) {
+		double a = Math.pow(3, 2);
+		double b = Math.pow(4, 2);
+		System.out.println("거리 = " + Math.sqrt(a + b));
+	}
+
+}
