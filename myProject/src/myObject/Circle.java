901f10bcf343866a9ef2801130f60b8afc457b59
Alder-R <kdh012356@gmail.com>
2024-09-05 09:51:03 +0900

    Update Circle.java
    정사각형 면적과 둘레 구하기

--- myProject/src/myObject/Circle.java
+++ myProject/src/myObject/Circle.java
@@ -5,23 +5,49 @@ abstract class ShapeExam {
 	public abstract double getCircum();		// 둘레
 }
 
-public class Circle extends ShapeExam {
-	double radius = 10;		// 원의 반지름
+public class Circle {
+	public static void main(String[] args) {
+		Circle2 c = new Circle2(10);
+		System.out.println("원의 반지름: " + c.radius);
+		System.out.println("원의 면적: " + c.getArea());
+		System.out.println("원의 둘레: " + c.getCircum());
+		
+		System.out.println();
+		
+		Square r = new Square(5);
+		System.out.println("정사각형의 한 변: " + r.edge);
+		System.out.println("정사각형의 면적: " + r.getArea());
+		System.out.println("정사각형의 둘레: " + r.getCircum());
+	}
+
+}
+
+class Circle2 extends ShapeExam {
+	double radius;		// 원의 반지름
+	public Circle2(double radius) {
+		this.radius = radius;
+	}
 	@Override
 	public double getArea() {		// 원의 면적
 		return Math.PI * radius * radius;
 	}
-
 	@Override
 	public double getCircum() {		// 원의 둘레
 		return Math.PI * 2 * radius;
 	}
+}
 
-	public static void main(String[] args) {
-		Circle c = new Circle();
-		System.out.println("원의 반지름: " + c.radius);
-		System.out.println("원의 면적: " + c.getArea());
-		System.out.println("원의 둘레: " + c.getCircum());
+class Square extends ShapeExam {
+	double edge;		// 정사각형의 한 변
+	public Square(double edge) {
+		this.edge = edge;
 	}
-
+	@Override
+	public double getArea() {		// 정사각형의 면적
+		return edge * edge;
+	}
+	@Override
+	public double getCircum() {		// 정사각형의 둘레
+		return edge * 4;
+	}
 }
