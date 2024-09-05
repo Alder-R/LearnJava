54e56c6e55877368d0b5d61626f612c57945fea9
Alder-R <kdh012356@gmail.com>
2024-09-05 12:12:14 +0900

    클래스 상속 학습 문제

---
+++ myProject/src/myObject/ExamMain.java
@@ -0,0 +1,55 @@
+package myObject;
+
+class Sports {
+	String name;
+	int player;
+	public Sports(String name, int player) {
+		this.player = player;
+		this.name = name;
+	}
+	public int getPlayer() {
+		return player;
+	}
+	public String getName() {
+		return name;
+	}
+	public void rule() {
+		System.out.println("경기 규칙 적는 곳");
+	}
+}
+
+class Baseball extends Sports {
+	public Baseball(String name, int player) {
+		super(name, player);
+	}
+	public void rule() {
+		System.out.println("야구 규칙");
+	}
+}
+
+class Basketball extends Sports {
+	public Basketball(String name, int player) {
+		super(name, player);
+	}
+	public void rule() {
+		System.out.println("농구 규칙");
+	}
+}
+
+public class ExamMain {
+	public static void main(String[] args) {
+		Baseball baseball = new Baseball("BaseBall", 9);
+		System.out.println("경기 종목 : " + baseball.getName());
+		System.out.println("선수 인원 : " + baseball.getPlayer() + "명");
+		System.out.print("경기 규칙 : ");
+		baseball.rule();
+		
+		System.out.println();
+		
+		Basketball basketball = new Basketball("Basketball", 5);
+		System.out.println("경기 종목 : " + basketball.getName());
+		System.out.println("선수 인원 : " + basketball.getPlayer() + "명");
+		System.out.print("경기 규칙 : ");
+		basketball.rule();
+	}
+}
