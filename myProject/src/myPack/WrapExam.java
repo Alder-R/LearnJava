17f4f9945a61109ea162b42e4b816c1a570ae601
Alder-R <kdh012356@gmail.com>
2024-09-26 10:53:04 +0900

    계속해서 Wrapper

--- myProject/src/myPack/WrapExam.java
+++ myProject/src/myPack/WrapExam.java
@@ -12,6 +12,18 @@ public class WrapExam {
 		System.out.println(in1);
 		System.out.println(in1.toString() + in2.toString());
 		System.out.println(in1 + in2);
+		
+		Double PI = new Double(3.14);
+		System.out.println(Double.toString(PI));
+		System.out.println(PI - .14 + "\t" + PI);
+		
+		String str1 = "30", str2 = "7.5";
+		// String값을 정수나 실수형 Wrapper로 변환
+		System.out.println(Integer.valueOf(str1) + Float.valueOf(str2));
+		System.out.println(str1 + str2);
+		
+		double i2 = Double.parseDouble(str2);
+		System.out.println(i2 + 2.4);
 	}
 
 }
